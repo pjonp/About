b19a866bc6da17c699b4800c9569cd99778113d3
pjonp <pjonp01@gmail.com>
2019-11-12 23:26:13 -0500

    Updates

---
+++ precache-manifest.ad213aac6be6b0dc53a5919817e8105d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78a169594371751c1628e0c87e1282f3",
+    "url": "/about/index.html"
+  },
+  {
+    "revision": "24a6627d9c33d577bae1",
+    "url": "/about/static/js/2.aa38d9e2.chunk.js"
+  },
+  {
+    "revision": "525b58131c7b7c7fc145",
+    "url": "/about/static/js/main.80a92c04.chunk.js"
+  },
+  {
+    "revision": "06b57208f66104fbad89",
+    "url": "/about/static/js/runtime-main.285f8918.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/about/static/media/logo.25bf045c.svg"
+  }
+]);
